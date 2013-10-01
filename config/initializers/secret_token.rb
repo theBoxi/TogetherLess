e1ee3c12deddac89e6af08a56fbd63aae32bdfd9
theBoxi <online@boxi.ch>
2013-10-02 00:54:55 +0200

    init empty RubyMineProject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TogetherLess::Application.config.secret_key_base = 'c9217eb497354160c420a5efb4786059374aced8f6ee40e4ef41f387e664c7b044591d186e319c4555513dc43f90e4fc96c108198d701e53c716fa43a1d8dfe5'
