296723c8f5781f0daeaea820d04d1715f54824ab
theBoxi <online@boxi.ch>
2013-11-30 17:08:31 +0100

    init HomePage (root :to)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TogetherLess::Application.routes.draw do
+  root :to => "home#show"
+  get "home" =>  "home#show"
+  #get "home/show"
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
