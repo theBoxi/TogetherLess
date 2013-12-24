1646782e0b34f50edb26c72a865abfb6a9fb343e
theBoxi <online@boxi.ch>
2013-12-24 23:46:10 +0100

    run on every server

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20131220115237) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
