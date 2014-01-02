7615d1d548dbe54e25d5c825db668df3a7db3c87
theBoxi <online@boxi.ch>
2014-01-02 11:07:15 +0100

    added username to login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,29 @@ class User < ActiveRecord::Base
   #attr_accessible :email, :password, :password_confirmation
 
   has_many :logs
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    unless @login
+      if self.username
+        self.username
+      else
+        self.email
+      end
+    else
+      @login
+    end
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131220115237) do
+ActiveRecord::Schema.define(version: 20140102083536) do
 
   create_table "logs", force: true do |t|
     t.integer  "user_id"
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 20131220115237) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
